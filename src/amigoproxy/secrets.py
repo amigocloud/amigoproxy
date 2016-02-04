a64bf787076dd13797a14b197c2a57c7d22ada04
Aldo Culquicondor <alculquicondor@gmail.com>
2016-02-04 10:27:07 -0500

    removing secrets

--- src/amigoproxy/secrets.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = 'g%r1%)4ax&q#2i&xqx*#=9+%al1wrma+4^*tkj&8h58c^!d6w)'
-
-HIPCHAT_TOKEN = '8a325f25adbaddc98682ea92398f1a'
-
-BROKER_URL = 'amqp://proxy_rabbitmq:passwordforproxy_rabbitmq@rabbitmq:5672//'
-
-REDIS_HOST = 'redis'
